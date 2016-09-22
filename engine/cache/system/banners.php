e621cdf6b66e937a6f362430da17a671cc82d5c8
Tumbleweed777 <tumbleweed777@gmail.com>
2016-09-22 19:40:07 +0300

    removed banner from static.tpl

--- engine/cache/system/banners.php
+++ engine/cache/system/banners.php
@@ -1,16 +1,1 @@
-a:2:{i:1;a:10:{s:2:"id";s:1:"1";s:10:"banner_tag";s:8:"tcc-full";s:5:"descr";s:8:"tcc-full";s:4:"code";s:26:"<div id="TCCB_3209"></div>";s:7:"approve";s:1:"0";s:11:"short_place";s:1:"0";s:6:"bstick";s:1:"0";s:4:"main";s:1:"0";s:8:"category";s:1:"0";s:10:"grouplevel";s:3:"all";}i:2;a:10:{s:2:"id";s:1:"2";s:10:"banner_tag";s:7:"adsense";s:5:"descr";s:7:"adsense";s:4:"code";s:510:"<center><table border="0" cellspacing="5" cellpadding="5" align="center">
-<tbody>
-<tr>
-<td><center><script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<!-- Zavtrak1 -->
-<ins class="adsbygoogle"
-     style="display:inline-block;width:336px;height:280px"
-     data-ad-client="ca-pub-1856950492454373"
-     data-ad-slot="1341230448"></ins>
-<script>
-(adsbygoogle = window.adsbygoogle || []).push({});
-</script></center></td>
-</tr>
-</tbody>
-</table></center>
-<br />";s:7:"approve";s:1:"1";s:11:"short_place";s:1:"0";s:6:"bstick";s:1:"0";s:4:"main";s:1:"0";s:8:"category";s:1:"0";s:10:"grouplevel";s:3:"all";}}
+a:0:{}
